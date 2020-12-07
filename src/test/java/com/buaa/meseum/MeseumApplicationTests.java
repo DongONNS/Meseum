dd36c32b12904a366dbd43e1d9b2422a9e54d91a
dongcheng2019 <dongcheng_2018@163.com>
2020-12-07 20:48:59 +0800

    博物馆小程序初步

---
+++ src/test/java/com/buaa/meseum/MeseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buaa.meseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeseumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
